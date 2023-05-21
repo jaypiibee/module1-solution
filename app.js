95e513dfede8eeab789dceee9f25aae387c98b04
John Paul Bagadiong <jbagadiong@magsaysay.com.ph>
2023-05-21 11:11:07 +0800

    modify

--- app.js
+++ app.js
@@ -1,41 +1,81 @@
+(function () {
+'use strict';
 
-// Define the modeule and controller
-angular.module('foodChecker',[]).controller('outputController', outputController);
+  // Define the modeule and controller
+  angular.module('foodChecker',[])
+  .controller('outputController', outputController)
+  .filter('custom', FilterCustomized);
 
-// Inject dependencies using array notation
-outputController.$inject = ['$scope'];
+  // Inject dependencies using array notation
+  outputController.$inject = ['$scope','$filter','customFilter'];
 
-// Define controller function
-function outputController($scope){
-  var a = this;
+  // Define controller function
+  function outputController($scope,$filter,customFilter){
+    var a = this;
+    var f = $filter;
+    var cf = customFilter;
 
-  a.foodItems = "";
-  a.message = "";
-  a.clicked = false;
+    a.otherMessage = "";
+    a.foodItems = "";
+    a.message = "";
+    a.clicked = false;
 
-  // Define checkFood function
-  a.checkFood = function(){
+    // Define checkFood function
+    a.checkFood = function(){
 
-    // Split input into array and remove empty string 
-    var foodArray = a.foodItems.split(",").filter(item => item.trim() !== "");
+      // Split input into array and remove empty string 
+      var foodArray = a.foodItems.split(",").filter(item => item.trim() !== "");
+      
+      if(!a.foodItems) {
+        a.message = "Please enter data first";
+        a.otherMessage = cf(a.message);
+        return a.UpperCase(a.message);
+      }
 
-    if(!a.foodItems) {
-      a.message = "Please enter data first";
-      return;
+      if(foodArray.length <= 3) {
+        a.message = "Enjoy";
+        a.changeColor();
+      }
+      else{
+        a.message = "To Much!";
+        a.changeColor();
+      }
+      a.otherMessage = cf(a.message);
+      
+      a.UpperCase(a.message);
+
+    };
+
+    a.UpperCase = function(messageOutput){
+        a.message = f('uppercase')(messageOutput);
     }
 
-    if(foodArray.length <= 3) {
-      a.message = "Enjoy";
-      a.changeColor();
-    }
-    else{
-      a.message = "To Much!";
-      a.changeColor();
-    }
+    a.changeColor = function(){
+      a.clicked = true;
+    };
 
-  };
+    // convert to other messsage using custom filter
+    
+  }
 
-  a.changeColor = function(){
-    a.clicked = true;
-  };
-}
+  function FilterCustomized() {
+    return function (input) {
+
+      if(input == "Enjoy"){
+        input = input.replace(input, "That's enough food")
+      }
+      else if (input == "To Much!"){
+        input = input.replace(input, "That's execive food")
+      }
+      else{
+        input = input.replace(input, "No Data input")
+      }
+
+      return input;
+    }
+  }
+
+})();
+
+//CustomFilters
+//WATCHERS
